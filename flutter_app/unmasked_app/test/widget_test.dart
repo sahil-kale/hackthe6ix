e61136f5f5b5b4320ff4bf2ec1c3590886e28662
Risat Haque <66147598+Risath18@users.noreply.github.com>
2020-08-22 13:52:43 -0600

    ADDING all files

--- flutter_app/unmasked_app/test/widget_test.dart
+++ flutter_app/unmasked_app/test/widget_test.dart
@@ -8,7 +8,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+<<<<<<< Updated upstream
 import 'package:unmasked_app/main.dart';
+=======
+import 'file:///C:/apps/hackthe6ix/flutter_app/unmasked/lib/home/main.dart';
+>>>>>>> Stashed changes
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
